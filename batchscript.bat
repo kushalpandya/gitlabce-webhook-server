c2e9a8abb78e0a3a6b907bbbaa812cdba72da33e
Kushal Pandya <kushalpandya@outlook.com>
2016-05-25 11:32:16 +0530

    Updated Hello message :fireworks:

--- batchscript.bat
+++ batchscript.bat
@@ -1,4 +1,4 @@
 REM Sample Windows Batch Script
-REM Change this script to run whatever commands you wish.
-echo "Hello World from Batch!"
+REM Change this script to run whatever commands you wish and include it in config.json.
+echo "Hello from GitLab CE Webhook Server!"
 git status
